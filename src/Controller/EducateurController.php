5b349bb7f1df23cd7e6b3e2f93ec42c29c746f6b
ChristianPeuffier <Christian.peuffier@etudiant.univ-rennes1.fr>
2023-12-28 23:22:11 +0100

    ajout crud Educateur

--- src/Controller/EducateurController.php
+++ src/Controller/EducateurController.php
@@ -2,17 +2,76 @@
 
 namespace App\Controller;
 
+use App\Entity\Educateur;
+use App\Form\EducateurType;
+use App\Repository\EducateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EducateurController extends AbstractController
 {
-    #[Route('/educateur', name: 'app_educateur')]
-    public function index(): Response
+    #[Route('/educateur', name: 'educateur.index', methods: ['GET'])]
+    public function index(EducateurRepository $repository, PaginatorInterface $paginator,Request $request): Response
     {
+        $educateurs = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            5
+        );
         return $this->render('pages/educateur/index.html.twig', [
-            'controller_name' => 'EducateurController',
+            'educateurs' => $educateurs
         ]);
     }
+
+    #[Route('/educateur/new', name: 'educateur.new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $educateur = new Educateur();
+        $form = $this->createForm(EducateurType::class, $educateur);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $educateur = $form->getData();
+            $manager->persist($educateur);
+            $manager->flush();
+            $this->addFlash('success', 'Le educateur a bien été ajoutée.');
+            return $this->redirectToRoute('educateur.index');
+        }
+        return $this->render('pages/educateur/new.html.twig',
+            [
+                'form' => $form->createView()
+            ]);
+    }
+
+    #[Route('/educateur/edition/{id}', name: 'educateur.edit', methods: ['GET', 'POST'])]
+    public function edit(Educateur $educateur, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(EducateurType::class, $educateur);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $educateur = $form->getData();
+            $manager->persist($educateur);
+            $manager->flush();
+            $this->addFlash('success', 'Le educateur a bien été modifiée.');
+            return $this->redirectToRoute('educateur.index');
+        }
+        return $this->render('pages/educateur/edit.html.twig',
+            [
+                'form' => $form->createView(),
+                'educateur' => $educateur
+            ]);
+    }
+
+    #[Route('/educateur/suppression/{id}', name: 'educateur.delete', methods: ['GET', 'POST'])]
+    public function delete(Educateur $educateur, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($educateur);
+        $manager->flush();
+        $this->addFlash('success', 'Le educateur a bien été supprimée.');
+        return $this->redirectToRoute('educateur.index');
+    }
+
 }
--- src/Form/EducateurType.php
+++ src/Form/EducateurType.php
@@ -2,42 +2,24 @@
 
 namespace App\Form;
 
-use App\Entity\Categorie;
-use App\Entity\Contact;
+use App\Entity\Educateur;
 use App\Entity\Licencie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
 
-class LicencieType extends AbstractType
+class EducateurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('numeroLicence', NumberType::class, [
-                'label' => 'Numéro de licence',
-                'label_attr' => [
-                    'class' => 'form-label mt-2'
-                ],
-                'attr' => [
-                    'class' => 'form-control',
-                    'min' => 0,
-                    'max' => 999999999999999,
-                    'placeholder' => 'Entrez votre numéro de licence'
-                ],
-                'constraints' => [
-                    new Assert\Positive([
-                        'message' => 'Le numéro de licence doit être un nombre positif.'
-                    ])
-                ]
-            ])
-            ->add('nom', TextType::class, [
-                'label' => 'Nom',
+            ->add('email',EmailType::class,[
+                'label' => 'Email',
                 'label_attr' => [
                     'class' => 'form-label mt-2'
                 ],
@@ -45,22 +27,22 @@ class LicencieType extends AbstractType
                     'class' => 'form-control',
                     'minlength' => 3,
                     'maxlength' => 50,
-                    'placeholder' => 'Entrez votre nom'
+                    'placeholder' => 'Entrez votre email'
                 ],
                 'constraints' => [
                     new Assert\Length([
                         'min' => 3,
                         'max' => 50,
-                        'minMessage' => 'Le nom doit contenir au moins {{ limit }} caractères',
-                        'maxMessage' => 'Le nom doit contenir au maximum {{ limit }} caractères'
+                        'minMessage' => 'L\'email doit contenir au moins {{ limit }} caractères',
+                        'maxMessage' => 'L\'email doit contenir au maximum {{ limit }} caractères'
                     ]),
                     new Assert\NotBlank([
-                        'message' => 'Le nom est obligatoire'
+                        'message' => 'L\'email est obligatoire'
                     ])
                 ]
             ])
-            ->add('prenom', TextType::class, [
-                'label' => 'Prénom',
+            ->add('password',TextType::class,[
+                'label' => 'Mot de passe',
                 'label_attr' => [
                     'class' => 'form-label mt-2'
                 ],
@@ -68,27 +50,24 @@ class LicencieType extends AbstractType
                     'class' => 'form-control',
                     'minlength' => 3,
                     'maxlength' => 50,
-                    'placeholder' => 'Entrez votre prénom'
+                    'placeholder' => 'Entrez votre mot de passe'
                 ],
                 'constraints' => [
                     new Assert\Length([
                         'min' => 3,
                         'max' => 50,
-                        'minMessage' => 'Le prénom doit contenir au moins {{ limit }} caractères',
-                        'maxMessage' => 'Le prénom doit contenir au maximum {{ limit }} caractères'
+                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
+                        'maxMessage' => 'Le mot de passe doit contenir au maximum {{ limit }} caractères'
                     ]),
                     new Assert\NotBlank([
-                        'message' => 'Le prénom est obligatoire'
+                        'message' => 'Le mot de passe est obligatoire'
                     ])
                 ]
             ])
-            ->add('categorie', EntityType::class, [
-                'class' => Categorie::class,
+            ->add('licencie', EntityType::class, [
+                'class' => Licencie::class,
 'choice_label' => 'nom',
-            ])
-            ->add('contact', EntityType::class, [
-                'class' => Contact::class,
-'choice_label' => 'email'
+
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer',
@@ -102,7 +81,7 @@ class LicencieType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Licencie::class,
+            'data_class' => Educateur::class,
         ]);
     }
 }
